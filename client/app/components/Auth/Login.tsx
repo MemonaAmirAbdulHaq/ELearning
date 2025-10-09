ee3cd1b3baa27333338a832cb0b9a4593a35fa63
Memona Amir <157165172+MemonaAmirAbdulHaq@users.noreply.github.com>
2025-10-09 12:41:26 +0500

    Login UI

--- client/app/components/Auth/Login.tsx
+++ client/app/components/Auth/Login.tsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { FC, useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { AiOutlinEye, AiOutlineInvisible, AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { styles } from "../../../app/styles/style";
+type Props = {
+  setRoute: (route: Props) => void;
+};
 
-type Props = {}
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid Email!")
+    .required("Please enter your email"),
+  password: Yup.string().required("Pleasr enter your password").min(6),
+});
+const Login: FC<Props> = (props: Props) => {
+  const [show, setShow] = useState(false);
+  const formik = useFormik({
+    initialValues: { email: "", password: "" },
+    validationSchema: schema,
+    onSubmit: async ({ email, password }) => {
+      console.log(email, password);
+    },
+  });
 
-const Login = (props: Props) => {
-  return (
-    <div></div>
-  )
-}
+  const{errors,touched,values,handleChange,handleSubmit}=formik;
+  return <div className="w-full">
+    <h1 className={`${styles.title}`}>
+     Login with ELearning
+    </h1>
+    <form onSubmit={handleSubmit}>
+        <label className={`${styles.label}`}
+        htmlFor="email">Enter your Email</label>
+        <input
+        type="email"
+        name=""
+        value={values.email}
+        onChange={handleChange}
+        id="email"
+        placeholder="loginmail@gmail.com"
+        className={`${
+            errors.email && touched.email && "border-red-500"
+        } ${styles.input}`}/>
+        {
+            errors.email && touched.email &&(
+                <span className="text-red-500 pt-2 block">
+                    {errors.email}
+                </span>
+         )
+        }
+        <div>
+            
+        </div>
+    </form>
+  </div>;
+};
 
-export default Login
+export default Login;
--- client/app/utils/CustomModal.tsx
+++ client/app/utils/CustomModal.tsx
@@ -18,8 +18,9 @@ const CustomModal:FC<Props> = ({open,setOpen,setRoute,component:Component}) => {
     aria-describedby="modal-modal-description"
     >
         <Box
-        className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[450px] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none"></Box>
+        className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[450px] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none">
     <Component setOpen={setOpen} setRoute={setRoute}/>
+   </Box>
     </Modal>
   )
 }
